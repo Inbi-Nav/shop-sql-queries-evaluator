f25d216d452ebe01908e0c6e4fd376e9f1d5c799
Inbi-Nav <156185561+Inbi-Nav@users.noreply.github.com>
2025-11-24 11:18:26 +0100

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -25,12 +25,19 @@ SELECT
 FROM producto;
 
 -- 6. Llista els noms (nombre) i els preus de tots els productes de la taula producto, convertint els noms a majúscula.
-
+SELECT UPPER(nombre) as nombre, precio
+FROM producto;
 
 -- 7. Llista els noms (nombre) i els preus (precio) dels productes de la taula producto, convertint els noms a minúscula.
-
+SELECT lower(nombre) as nombre, precio
+FROM producto;
 
 -- 8. Llista el nom de tots els fabricants en una columna, i en una altra columna obtingui en majúscules els dos primers caràcters del nom del fabricant (iniciales).
+SELECT nombre, 
+		CONCAT(
+		UPPER(SUBSTRING(nombre, 1, 2)), 
+        SUBSTRING(nombre, 3)) as nombre 
+FROM fabricante;
 
 
 -- 9. Llista els noms i els preus dels productes, arrodonint el valor del preu (precio)
